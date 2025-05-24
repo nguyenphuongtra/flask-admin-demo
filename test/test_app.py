7019ba2a7beb04c8941d4d01faa3faa782144b21
Your Name <you@example.com>
2025-05-24 13:59:18 +0700

    update ci first

--- test/test_app.py
+++
@@ -1,7 +0,0 @@
-from app import app
-
-def test_test_page():
-    client = app.test_client()
-    response = client.get('/test')
-    assert response.status_code == 200
-    assert b"This is a test page!" in response.data
